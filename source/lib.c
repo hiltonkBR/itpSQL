786e122d69bc6a03da6f09a67edf41b858e0516a
hiltonkBR <hilton.kbr@gmail.com>
2018-11-12 00:54:00 -0300

    Inserção msg de sucesso

--- source/lib.c
+++ source/lib.c
@@ -2,20 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-void writeFile(char *tableName, char *itemTable, char *operation)
-{
-  
-  FILE *arq, *allTables;
-
-
-  arq = fopen(tableName, operation);
-
-  fprintf(arq, " %s ", itemTable);
-
-  fclose(arq);
-}
-
+int tes;
 
 int criarTabela(){
         int qtd_colunas, n, qtd_lines; 
@@ -41,14 +28,12 @@ int criarTabela(){
 	
 	fprintf(estruTabela, "%s\n", nomeTabela);
 	fprintf(dadosTabela, "%s\n", nomeTabela);
-        //fclose(estruTabela);
         fclose(dadosTabela);
 
 
 	printf("Digite a quantidade de colunas:\n");
 	scanf("%d", &qtd_colunas);
 	n = qtd_colunas;
-	int i = 0;
 	char tipo[100], nomeCOL[100], insertCOL[100];
 
 
@@ -56,36 +41,35 @@ int criarTabela(){
 		
 		if(j == 1){   
 			printf("Digite o nome de sua chave primaria %d : ", j);
-                        scanf("%s", nomeCOL); //Esse seria o array das variaveis
-                        //printf("1 - Int / 2 - Double / 3 - Float / 4 - String\n");
-                        //printf("Digite o tipo de variavel da coluna [%d] :", j);
-                        //scanf("%s", tipo); //Esse seria o array das variaveis	
+                        scanf("%s", nomeCOL); 
 			strcpy(tipo, "1");
 		}else{
 			printf("Digite o nome da coluna %d : ", j);		
-			scanf("%s", nomeCOL); //Esse seria o array das variaveis
+			scanf("%s", nomeCOL); 
 			printf("1 - Int / 2 - Double / 3 - Float / 4 - String\n");
 			printf("Digite o tipo de variavel da coluna [%d] :", j);
-			scanf("%s", tipo); //Esse seria o array das variaveis
+			scanf("%s", tipo); 
 		}
 		strcat(tipo, " | ");
 		strcat(tipo, nomeCOL);
 		fprintf(estruTabela, "%s\n", tipo);
-	
-	i++;
 	}
-	fclose(estruTabela);	
-
+	fclose(estruTabela);
+	tes= 10;	
 }
 
 
-void menu()
-{
-    printf("---------------- itpSQL ----------------\n");
-    printf("--------- ITP/PTP 2018.2 N12 -----------\n");
-    printf("     Feito por Hilton & Hasler\n");
-    printf("\n");
-
+void menu(){
+	if(tes ==10){
+ 		printf("----------------------------------------\n");
+ 		printf("----- Tabela inserida com sucesso ------\n");
+ 		printf("----------------------------------------\n\n");
+	}
+    	printf("---------------- itpSQL ----------------\n");
+    	printf("--------- ITP/PTP 2018.2 N12 -----------\n");
+    	printf("     Feito por Hilton & Hasler\n");
+    	printf("\n");
+tes = 0 ;
     int option;
     printf("selecione uma opção:\n1. Criar uma Tabela\n2. Listar tabelas\n3. Inserir registros em uma tabela\n4. Mostrar dados de uma tabela\n5. Pesquisar em uma tabela\n6. Apagar valor de uma Tabela\n7. Apagar uma Tabela\n" );
 
@@ -95,9 +79,11 @@ void menu()
       switch (option)
       {
         case 1:
-          criarTabela();
-          printf ("\n");
-        break;
+        criarTabela();
+        printf ("\n");
+        system("clear");
+        menu();
+	break;
 
         case 2 :
           //insertItensAfterFile();
